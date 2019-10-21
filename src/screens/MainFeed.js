965f80029d558aec221144be5397fc10455f2b55
alecfn <apfor2@student.monash.edu>
2019-10-21 22:24:26 +1100

    Getting reddit post titles but not rendering in list yet

--- src/screens/MainFeed.js
+++ src/screens/MainFeed.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, FlatList, StyleSheet } from 'react-native';
 
 export default class MainFeed extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      homePageArray: [],
+      isLoaded: false,
+    }
+  }
+
+  // For now, just grab the front page JSON so it can be put into views.
+  componentDidMount() {
+    fetch("https://www.reddit.com/.json")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          homePageArray: json,
+        })
+      });
+  }
+
   render() {
-    return (
-      <View>
-        <Text>New page</Text>
-      </View>
-    )
+
+    // Load these values so we can access them while rendering.
+    var { isLoaded, homePageArray } = this.state;
+
+    if (!isLoaded) {
+      return <Text>Loading... </Text>
+    }
+    else {
+      // Put all the post titles in their own array for easy access.
+      var posts = [];
+      this.state.homePageArray.data.children.forEach(child =>
+        posts.push(child.data.title));
+      console.log(posts)
+      return (
+        <FlatList
+          data={posts}
+      //    renderItem={()}
+        />
+      );
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
