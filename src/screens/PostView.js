2ef48509cfc44d336db509cb6e73c52b0a56527d
alecfn <apfor2@student.monash.edu>
2019-10-27 14:41:28 +1100

    Just fetching JSON for posts doesn't seem to work, Will probably need to start doing some proper GET requests instead

--- src/screens/PostView.js
+++ src/screens/PostView.js
@@ -25,7 +25,7 @@ export default class PostView extends Component {
     try {
       let response = await fetch(postUrlJsonEndpoint);
       this.setState({
-        postJson: response.json(),
+        postJson: JSON.stringify(response.json()),
         isLoaded: true
       })
     } catch(err) {
@@ -42,6 +42,7 @@ export default class PostView extends Component {
       return <Text>Loading post...</Text>
     }
     else {
+      console.log(postJson)
       return (
         <Text>{postUrl}</Text>
       );
